b41bbeca25055717757acdd10c345ecef45195b4
hrhabib0 <hrhabib398668@gmail.com>
2025-06-21 22:04:27 +0600

    create single data api and send response to the client side

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 3000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 // middleWar
@@ -42,6 +42,13 @@ async function run() {
             const cursor = productsCollections.find();
             const result = await cursor.toArray();
             res.send(result)
+        })
+        // get single product api
+        app.get('/products/:id', async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id : new ObjectId(id)};
+            const result = await productsCollections.findOne(query);
+            res.send(result);
         })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
